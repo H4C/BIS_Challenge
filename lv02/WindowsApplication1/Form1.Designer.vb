f23d3b4b1ab01c2685a4d9f70c0d3d1ed197ae80
PhilipMo <nodezero@naver.com>
2016-03-02 15:03:39 +0900

    lv02
    :+1:

---
+++ lv02/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.txtPW = New System.Windows.Forms.TextBox()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.lblUserName = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnCancel
+        '
+        Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancel.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnCancel.Location = New System.Drawing.Point(116, 124)
+        Me.btnCancel.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(95, 35)
+        Me.btnCancel.TabIndex = 26
+        Me.btnCancel.Text = "Exit"
+        '
+        'btnOK
+        '
+        Me.btnOK.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnOK.Location = New System.Drawing.Point(15, 124)
+        Me.btnOK.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(95, 35)
+        Me.btnOK.TabIndex = 25
+        Me.btnOK.Text = "Login"
+        '
+        'txtPW
+        '
+        Me.txtPW.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtPW.Location = New System.Drawing.Point(16, 90)
+        Me.txtPW.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtPW.Name = "txtPW"
+        Me.txtPW.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPW.Size = New System.Drawing.Size(195, 26)
+        Me.txtPW.TabIndex = 24
+        '
+        'txtID
+        '
+        Me.txtID.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtID.Location = New System.Drawing.Point(15, 35)
+        Me.txtID.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(196, 26)
+        Me.txtID.TabIndex = 22
+        '
+        'lblPassword
+        '
+        Me.lblPassword.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblPassword.Location = New System.Drawing.Point(14, 65)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(193, 28)
+        Me.lblPassword.TabIndex = 23
+        Me.lblPassword.Text = "PW"
+        Me.lblPassword.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblUserName
+        '
+        Me.lblUserName.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblUserName.Location = New System.Drawing.Point(12, 9)
+        Me.lblUserName.Name = "lblUserName"
+        Me.lblUserName.Size = New System.Drawing.Size(195, 28)
+        Me.lblUserName.TabIndex = 21
+        Me.lblUserName.Text = "ID"
+        Me.lblUserName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(229, 183)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.txtPW)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.lblUserName)
+        Me.Name = "Form1"
+        Me.Text = "ByPass"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents btnOK As Button
+    Friend WithEvents txtPW As TextBox
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents lblUserName As Label
+End Class
---
+++ lv02/WindowsApplication1/Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text
+Public Class Form1
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        If txtID.Text = My.Resources.id Then
+            If GetHash(txtPW.Text) = My.Resources.pw Then
+                MessageBox.Show("Cftu Pg Uif Cftu")
+
+            Else
+                    Environment.Exit(0)
+            End If
+        Else
+            Environment.Exit(0)
+        End If
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Environment.Exit(0)
+    End Sub
+
+    Shared Function GetHash(theInput As String) As String
+
+        Using hasher As MD5 = MD5.Create()
+
+
+            Dim insu As Byte() = hasher.ComputeHash(Encoding.UTF8.GetBytes(theInput))
+
+            Dim babo As New StringBuilder()
+
+            For n As Integer = 0 To insu.Length - 1
+                babo.Append(insu(n).ToString("X2"))
+            Next n
+
+            Return babo.ToString()
+        End Using
+
+    End Function
+End Class
